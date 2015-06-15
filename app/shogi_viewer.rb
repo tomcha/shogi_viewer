ee9a05c2c93980bcc22865e4ff1bba59871993f9
tomcha <tomcha@tomcha.net>
2015-06-15 20:33:34 +0900

    棋譜ブラウザ表示コード追加

--- app/shogi_viewer.rb
+++ app/shogi_viewer.rb
@@ -2,10 +2,15 @@
 
 require 'sinatra/base'
 require 'haml'
+require 'yaml'
 
 class Shogi_viewer < Sinatra::Base
   set :public_folder, File.expand_path(File.join(root, '..', 'public'))
   get '/' do
+    File.open('./public/data/kifu.data') do |file|
+      @data = YAML.load(file)
+    end
+    p @data
     haml :index
   end
 end
